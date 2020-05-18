6f55e87670b6288481a360bf8d244c92b32c87c6
absolutesingh <sehajpreet.singh023@gmail.com>
2020-05-18 23:18:48 +0530

    May 18

---
+++ JavaWorkspace/JavaDS/src/accessmodifiers/first.java
@@ -0,0 +1,17 @@
+package accessmodifiers;
+
+public class first {
+	private int a =5;
+	int b = 10;
+	protected int c =15;
+	public int d = 20;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		first f = new first();
+		System.out.println(f.a);
+		System.out.println(f.b);
+		System.out.println(f.c);
+		System.out.println(f.d);
+	}
+
+}
---
+++ JavaWorkspace/JavaDS/src/accessmodifiers2/fourth.java
@@ -0,0 +1,13 @@
+package accessmodifiers2;
+import accessmodifiers.first;
+public class fourth {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		first f = new first();
+//		System.out.println(f.a); //private field
+//		System.out.println(f.b); //default 
+//		System.out.println(f.c);
+		System.out.println(f.d);
+	}
+
+}
