39c4d622b151a4a441a9783e32968f93042cdfee
absolutesingh <sehajpreet.singh023@gmail.com>
2020-06-06 20:09:27 +0530

    June 05

---
+++ JavaWorkspace/JavaDS/src/queues/queueusingstack.java
@@ -0,0 +1,43 @@
+package queues;
+
+public class queueusingstack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	//Dequeue Costly
+//	class StackQueue
+//	{
+//	    Stack<Integer> s1 = new Stack<Integer>();
+//	    Stack<Integer> s2 = new Stack<Integer>();
+//
+//	    /* The method insert to push element
+//	       into the queue */
+//	    void insert(int B)
+//	    {
+//		   // Your code here
+//		   s1.push(B);
+//	    }
+//		
+//	    
+//	    /* The method remove which return the
+//	      element popped out of the queue*/
+//	    int remove()
+//	    {
+//		   if(s1.isEmpty()) return -1;
+//		   while(s1.size()!=1)
+//		   {
+//		       s2.push(s1.pop());
+//		   }
+//		   int x = s1.pop();
+//		   while(!s2.isEmpty()){
+//		       s1.push(s2.pop());
+//		   }
+//		   return x;
+//	    }
+//	}
+
+	
+	//Enqueue Costly
+}
