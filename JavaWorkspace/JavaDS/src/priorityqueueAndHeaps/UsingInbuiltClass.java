a2c1e3a5531a133dea1061c035e30e34359c0de6
Sehajpreet Singh <sehajpreet.singh023@gmail.com>
2020-06-15 22:55:59 +0530

    June 15 PQ & Heaps

---
+++ JavaWorkspace/JavaDS/src/priorityqueueAndHeaps/UsingInbuiltClass.java
@@ -0,0 +1,23 @@
+package priorityqueueAndHeaps;
+
+import java.util.PriorityQueue;
+
+public class UsingInbuiltClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		pq.add(3);
+		pq.add(4);
+		pq.add(1);
+		pq.add(9);
+		pq.add(7);
+		pq.add(2);
+		System.out.println(pq.peek());//by default this is a min PriorityQueue
+		System.out.println(pq.poll());
+		System.out.println(pq.isEmpty());
+		System.out.println(pq.size());
+		System.out.println(pq.poll());
+	}
+
+}
